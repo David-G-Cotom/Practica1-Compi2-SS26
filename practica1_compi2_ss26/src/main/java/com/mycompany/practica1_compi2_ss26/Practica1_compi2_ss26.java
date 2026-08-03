de8cbde3088a37180a758a20a66bfcdb4ace7e59
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2026-08-03 14:42:29 -0600

    Project creation using Netbeans and Maven.

---
+++ practica1_compi2_ss26/src/main/java/com/mycompany/practica1_compi2_ss26/Practica1_compi2_ss26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1_compi2_ss26;
+
+/**
+ *
+ * @author david
+ */
+public class Practica1_compi2_ss26 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
